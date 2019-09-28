64fdb81ba85ac34170cb7d95447675379952bb60
Stefanie Reyes <sreyes8@binghamton.edu>
2019-09-28 11:55:34 -0400

    Created models

--- db/models/test.js
+++ db/models/test.js
@@ -1,8 +1,22 @@
-'use strict';
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+"use strict";
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define();
+const Test = db.define("test", {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+//Setting associations
+
+Student.hasMany(Test);
+Test.belongsTo(Student);
 
 module.exports = Test;
